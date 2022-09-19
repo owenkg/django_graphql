a6bd013dc0770b4d063a46adf23670319b7e6c4a
gokivumbi <okivumbi@sailglobalcorp.com>
2022-09-19 18:21:03 +0300

    refactor: resolve all books

--- books/schema.py
+++ books/schema.py
@@ -13,5 +13,8 @@ class Query(graphene.ObjectType):
 
     all_books = graphene.List(BooksType)
 
+    def resolve_all_books(root, info):
+        return Books.objects.all()
+
 
 schema = graphene.Schema(query=Query)
