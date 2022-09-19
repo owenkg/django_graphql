b15a4030f34d470c06015abac4641e976ec620d4
gokivumbi <okivumbi@sailglobalcorp.com>
2022-09-19 18:28:58 +0300

    refactor: added commented return for filter types

--- books/schema.py
+++ books/schema.py
@@ -14,6 +14,7 @@ class Query(graphene.ObjectType):
     all_books = graphene.List(BooksType)
 
     def resolve_all_books(root, info):
+        # return Books.objects.filter(title="django")
         return Books.objects.all()
 
 
