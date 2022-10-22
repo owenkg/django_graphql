d14b67b96cb8c34e9c55d9a4f1ec3db75459f952
gokivumbi <okivumbi@sailglobalcorp.com>
2022-10-22 15:02:18 +0300

    refactor: formatting shema file

---
+++ quiz/schema.py
@@ -0,0 +1,28 @@
+import graphene
+from graphene_django import DjangoObjectType
+from graphene_django import DjangoListField
+from .models import Quizzes, Question, Category, Answer
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+        fields = ("id", "name")
+
+
+class QuizzesType(DjangoObjectType):
+    class Meta:
+        model = Quizzes
+        fields = ("id", "title", "category", "name")
+
+
+class QuestionType(DjangoObjectType):
+    class Meta:
+        model = Question
+        fields = ("title", "quiz")
+
+
+class AnswerType(DjangoObjectType):
+    class Meta:
+        model = Answer
+        fields = ("question", "answer_text")
